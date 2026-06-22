8a00d25c821d758c7068435845d151780e162fa1
steve1676 <leroux.steven16@gmail.com>
2026-06-23 00:13:11 +0200

    météo

--- DashBoard/script.js
+++ DashBoard/script.js
@@ -25,7 +25,7 @@ function getWeatherIcon(code) {
     const isNight = hour < 7 || hour >= 21;
 
     if ([0].includes(code))                         return isNight ? "🌕" : "☀️";
-    if ([1, 2, 3].includes(code))                   return isNight ? "🌙" : "⛅";
+    if ([1, 2, 3].includes(code))                   return isNight ? "☁️" : "⛅";
     if ([45, 48].includes(code))                    return "🌫️";
     if ([51, 53, 55, 61, 63, 65].includes(code))    return "🌧️";
     if ([71, 73, 75, 77].includes(code))            return "❄️";
